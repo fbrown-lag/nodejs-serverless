68c073018d7861beca2a3070488aa4589a1f134c
Florian Brown <florian.brown@ac-grenoble.fr>
2025-02-07 10:25:21 +0100

    add parameters

--- api/hello.ts
+++ api/hello.ts
@@ -2,7 +2,9 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
-  return res.json({
+    return res.json({
+        parameters : `name`,
+      name : name,
     message: `Hello ${name}!`,
   })
 }
